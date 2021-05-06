7aab74adbc0a9a5cab640b7ced2e06d8776868c3
Gennan <e195735@ie.u-ryukyu.ac.jp>
2021-05-06 15:51:03 +0900

    演習1.1

--- dataset1.py
+++ dataset1.py
@@ -10,7 +10,16 @@ def true_function(x):
     return y
 
 
+# テスト
 if __name__ == '_main_':
     import doctest
 
     doctest.testmod()
+
+# 描画
+import matplotlib.pyplot as plt
+
+x = np.arange(-1, 1, 0.1)
+y = np.sin(np.pi * x * 0.8)
+plt.plot(x, y)
+plt.show()
